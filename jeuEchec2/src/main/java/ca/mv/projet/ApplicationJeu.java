99939e65e673efc7cf8ee34b5f8674b1fb54f402
Mathis1187 <mathisvincent1@gmail.com>
2024-05-09 19:08:44 -0400

    supprime de chose inutile

--- jeuEchec2/src/main/java/ca/mv/projet/ApplicationJeu.java
+++ jeuEchec2/src/main/java/ca/mv/projet/ApplicationJeu.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
